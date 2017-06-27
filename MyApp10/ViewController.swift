fe0441e84017e794277a63ffe624672abcf13a35
iii <iii@userteki-macbook.local>
2017-06-27 14:34:26 +0800

    v1

--- MyApp10/ViewController.swift
+++ MyApp10/ViewController.swift
@@ -10,16 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBAction func getImage(_ sender: Any) {
+    
+        DispatchQueue.global().async {
+            self.fetchImage()
+        }
+    }
+    
+    @IBAction func testGet(_ sender: Any) {
+        do {
+            let url = URL(string: "http://10.2.1.134/brad.php")
+            let source = try String(contentsOf: url!,
+                                    encoding: String.Encoding.ascii)
+            
+            print(source)
+            
+        }catch {
+            print("OKOKO")
+            // 處理 try 的錯誤
+            print(error)
+        }
+
+        
+        
+    }
+    private func fetchImage(){
+        do {
+            let url = URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV4_0A-adKUPF53giGqliOVGW7P0ctXZ5g5BQil0AVZ4BHBhhK")
+            if url != nil  {
+                let data = try Data(contentsOf: url!)
+                let img = UIImage(data: data)
+                DispatchQueue.main.async {
+                    self.imgView.image = img
+                }
+            }else {
+                let img = UIImage(named: "user")
+                DispatchQueue.main.async {
+                    self.imgView.image = img
+                }
+            }
+        }catch{
+            let img = UIImage(named: "user")
+            DispatchQueue.main.async {
+                self.imgView.image = img
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            let url = URL(string: "https://www.google.com/")
+            let source = try String(contentsOf: url!,
+                                    encoding: String.Encoding.ascii)
+            
+            //print(source)
+            
+        }catch {
+            print("OKOKO")
+            // 處理 try 的錯誤
+            print(error)
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
